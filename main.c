20ad428fc73c83f957a03202eb84005148152623
Tyler Gabb <tylerjgabb@gmail.com>
2023-12-16 20:23:47 +0000

    simple socket mirror

--- main.c
+++ main.c
@@ -1,26 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h> // Include the <unistd.h> header file for the close() function
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("No argument provided.\n");
-        return 1;
+int main()
+{
+    int sockfd;
+    struct sockaddr_in server_addr;
+    char buffer[1024];
+
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Get the length of the argument
-    int length = strlen(argv[1]);
+    // Set server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Allocate memory for the buffer
-    char *buffer = (char *)malloc((length + 1) * sizeof(char));
+    // Bind socket to the specified port
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Socket binding failed");
+        exit(EXIT_FAILURE);
+    }
 
-    sscanf(argv[1], "%s", buffer);
+    printf("Socket opened on port 8080\n");
 
-    // Print the buffer
-    printf("Argument: %s\n", buffer);
+    // Listen for incoming connections
+    if (listen(sockfd, 5) < 0)
+    {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
 
-    // Free the buffer
-    free(buffer);
+    while (1)
+    {
+        // Accept incoming connection
+        int client_sockfd;
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_sockfd < 0)
+        {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Read data from the socket
+        int bytes_read = recv(client_sockfd, buffer, sizeof(buffer), 0);
+        if (bytes_read < 0)
+        {
+            perror("Read failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // if the request body contains ?query=stop then stop the server
+        if (strstr(buffer, "?query=stop") != NULL)
+        {
+            printf("Stopping server\n");
+            close(sockfd);
+            exit(EXIT_SUCCESS);
+        }
+
+        // Prepare the HTML response
+        char response[2048];
+        snprintf(response, sizeof(response), "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body>%s</body></html>", buffer);
+
+        // Send the HTML response back
+        int bytes_sent = send(client_sockfd, response, strlen(response), 0);
+        if (bytes_sent < 0)
+        {
+            perror("Send failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Sent Mirror\n");
+
+        // Close the client connection
+        close(client_sockfd);
+    }
+    close(sockfd);
 
     return 0;
 }
